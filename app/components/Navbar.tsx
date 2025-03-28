18199faeb839397273bdf62c32d8236357923c8f
ganeshkondaka <kondakaganesh2003@outlook.com>
2025-03-28 12:58:56 +0530

    added landing page

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div className='pt-4 flex justify-between items-center'>
+            <div className='flex items-center '>
+                <img src="/logo2.png" alt="logo" width={50} height={50} />
+                <p className='text-2xl font-bold px-2'>EventHive</p>
+            </div>
+            <Link
+                href={'/login'}
+                className='px-5 py-1 rounded-lg bg-violet-600 text-xs md:text-lg'>Login</Link>
+        </div>
+    )
+}
+
+export default Navbar
